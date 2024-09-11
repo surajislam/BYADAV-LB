0703ed91211a270f4bd97cfca1c5b55747d558b1
surajislam <154444093+surajislam@users.noreply.github.com>
2024-09-11 15:30:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/surajislam/LB_Music",
+    "https://github.com/surajislam/Ayadav_LB",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
